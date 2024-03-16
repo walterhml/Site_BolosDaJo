7fb2cdf8cc2eda1fbdd71427387c23bf83df9a2b
walter souza <wszin06@gmail.com>
2024-03-16 00:43:28 -0300

    form do php

---
+++ src/php/contato.php
@@ -0,0 +1,32 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = strip_tags(trim($_POST["name"]));
+        $name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["message"]);
+
+        if (empty($name) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            http_response_code(400);
+            echo "Please complete the form and try again.";
+            exit;
+        }
+
+        $recipient = "walthersouza144@gmail.com";
+        $subject = "New contact from $name";
+        $email_content = "Name: $name\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Message:\n$message\n";
+
+        if (mail($recipient, $subject, $email_content)) {
+            http_response_code(200);
+            echo "Thank you! Your message has been sent.";
+        } else {
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
+
+    } else {
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
+?>
